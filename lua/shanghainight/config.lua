ae9ba6605cf221b01daa66dc611e75aa032e1457
Nick deLannoy <nick.delannoy@gmail.com>
2024-07-05 15:08:30 -0400

    wip

--- lua/shanghainight/config.lua
+++ lua/shanghainight/config.lua
@@ -6,7 +6,7 @@ M.version = "4.3.0" -- x-release-please-version
 ---@field on_colors fun(colors: ColorScheme)
 ---@field on_highlights fun(highlights: shanghainight.Highlights, colors: ColorScheme)
 M.defaults = {
-  style = "moon", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+  style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
   light_style = "day", -- The theme is used when the background is set to light
   transparent = false, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
